3f1d24d70c681f7b0c3619f20a14bb576847177e
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-19 13:03:00 +0200

    Add project files.

---
+++ 01 Välkomstsnurran/Program.cs
@@ -0,0 +1,16 @@
+﻿while(true)
+{
+    Console.WriteLine("Vill du fortsätta snurra, skriv ja/j");
+    string keepLooping = Console.ReadLine().ToLower();
+
+    if(keepLooping != "ja" &&  keepLooping != "j")
+    {
+        Console.WriteLine("Avslutar loopen!!!");
+        break;
+    }
+    Console.WriteLine("Fortsätter snurra.........");
+
+}
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen....");
+Console.ReadKey();
