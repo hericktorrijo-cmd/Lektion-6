d255adc386dac80180f676ec2f46e162c2ac3916
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-19 13:13:22 +0200

    uppgift 2 klar

---
+++ 02 Inmatningsskyddet/Program.cs
@@ -0,0 +1,23 @@
+﻿string correctPass = "hemligt123";
+
+string userAnswer;
+
+do
+{
+    Console.Write("Skriv in lösenord: ");
+    userAnswer = Console.ReadLine();
+
+    if (userAnswer != correctPass)
+    {
+        Console.WriteLine("Fel lösenord, försök igen...");
+        userAnswer = Console.ReadLine();
+
+    }
+
+}
+while (userAnswer != correctPass);
+
+Console.WriteLine("Korrekt lösenord! Välkommen!");
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
