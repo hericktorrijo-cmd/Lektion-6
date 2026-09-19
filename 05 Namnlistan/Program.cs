498db1862521c32af79568ed19e550d5c66b37ed
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-19 13:32:05 +0200

    Uppgift 5 klar

---
+++ 05 Namnlistan/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] names = { "Herick", "Chris", "Smiley", "Joker", "Chico" };
+
+foreach  (string name in names)
+{
+    Console.WriteLine(name.ToUpper());
+}
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
