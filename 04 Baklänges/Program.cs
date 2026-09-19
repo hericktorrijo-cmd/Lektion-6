315dade380be01cec50309d40a153a0384d1724f
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-19 13:26:47 +0200

    uppgift 4 klar

---
+++ 04 Baklänges/Program.cs
@@ -0,0 +1,9 @@
+﻿for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Gott nytt år!");
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
