9555b6f32c6104db786244e783c4a17e3d09bb72
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-19 20:05:47 +0200

    Uppgift 7 klar

---
+++ 07 Hoppa över dolda filer/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] fileNames = [".config", ".index.html", ".git", "styles.css", "Excel", "CSS"];
+
+foreach (string fileName in fileNames)
+{
+    if (fileName.StartsWith("."))
+        {
+        continue;
+        }
+
+    Console.WriteLine(fileName);
+}
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
