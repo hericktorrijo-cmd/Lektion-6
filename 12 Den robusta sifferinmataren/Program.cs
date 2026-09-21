471e6db7b105db1abaa7bc94c123dec8b18eb77f
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-21 20:53:14 +0200

    Sista uppgiften klar

---
+++ 12 Den robusta sifferinmataren/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Skriv in ett heltal: ");
+
+int intNumber;
+
+while (!int.TryParse(Console.ReadLine(), out intNumber))
+{
+    Console.WriteLine("FEL! Försök igen");
+    Console.WriteLine("Skriv in ett heltal: ");
+}
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
