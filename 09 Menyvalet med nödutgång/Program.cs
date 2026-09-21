bfb91bcec3372042a2450328ca723865692e4a5e
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-21 19:41:47 +0200

    Uppgift 9 klar

---
+++ 09 Menyvalet med nödutgång/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+while (true)
+{
+    Console.WriteLine("1: Visa profil");
+    Console.WriteLine("2: Ändra inställningar");
+    Console.WriteLine("3: Avsluta");
+    
+    string userChoice = Console.ReadLine();
+
+    if(userChoice == "3")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
