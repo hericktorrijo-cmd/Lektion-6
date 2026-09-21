b678f621b26c21923ca2a7954e9c5cced2d0d476
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-21 19:14:26 +0200

    uppgift 8 klar

---
+++ 08 Multiplikationstabellen/Program.cs
@@ -0,0 +1,12 @@
+﻿for(int yttre = 1; yttre <= 5;  yttre++)
+{
+    
+    for(int inre = 1; inre <=5; inre++)
+    {
+        Console.Write($"{inre * yttre,-4}");
+    }
+    Console.WriteLine(" ");
+}
+
+Console.WriteLine("\n\nTryck på valfri knapp för att stänga konsolen...");
+Console.ReadKey();
