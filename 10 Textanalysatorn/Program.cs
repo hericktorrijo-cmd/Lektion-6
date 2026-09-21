7e1b386dc69249f448e7911f374a28e6cb99f97d
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-21 20:44:34 +0200

    Uppgift 11 klar

---
+++ 10 Textanalysatorn/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Skriv en valfri text, vi ska beräkna antal mellanslag i meningen: ");
+string userText = Console.ReadLine();
+
+int spacebar = 0;
+
+foreach (char letter in userText)
+{
+    if(letter == ' ')
+    {
+        spacebar++;
+    }
+}
+
+Console.WriteLine($"Antal mellanslag: {spacebar}");
+
+Console.WriteLine("\n\nTryck på valfri knapp för att stänga konsolen...");
+Console.ReadKey();
---
+++ 11 Temperaturloggen/Program.cs
@@ -0,0 +1,23 @@
+﻿double[] temperatures = { 23.4, 25.7, 28.9, 30.1, 22.0 };
+double totalTemperature = 0;
+double highestTemperature = temperatures[0];
+double avarageTemperature = 0;
+foreach (double temperature in temperatures)
+{
+    totalTemperature += temperature;
+
+    if (temperature > highestTemperature)
+    {
+        highestTemperature = temperature;
+    }
+
+
+}
+
+avarageTemperature = totalTemperature / temperatures.Length;
+
+Console.WriteLine($"Medeltemperaturen är: {avarageTemperature:F1}");
+Console.WriteLine($"Högsta temperaturen är: {highestTemperature:F1}");
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
